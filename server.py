cc0011a249280850107e863d269d2c9dbb093c95
Robert <robert.crager18@gmail.com>
2021-03-26 03:11:50 -0500

    Server initial backend developed.

--- server.py
+++ server.py
@@ -1,5 +1,37 @@
-# Server backend for the Vaste application
-import socket
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+# Handle connections for the Vaste server.
 
-HOST, PORT = '', 8080
-listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.protocol_version = "HTTP/1.1"
+        self.send_response(200)
+        file = ""
+        if self.path.endswith("/"):
+            self.path += "index.html"
+        elif (not self.path.endswith("/index.html")):
+            self.path += "/index.html"
+        try:
+            file = open("./html" + self.path, "rb")
+        except FileNotFoundError:
+            file = open("./html/404.html","rb")
+        file.seek(0, 2)
+        self.send_header("Content-Length", file.tell())
+        self.end_headers()
+        file.seek(0)
+        self.wfile.write(file.read())
+        print("./html" + self.path)
+        file.close()
+        return
+
+def run():
+    HOST, PORT = "localhost", 8080
+    try:
+        server = (HOST, PORT)
+        httpd = HTTPServer(server, RequestHandler)
+        print(f"Server started on port {PORT} at host {HOST}.")
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+        httpd.shutdown()
+run()
