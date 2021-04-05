fdf4fb8629b7fc4d0020d5d83e6e4469042d211b
Robert <robert.crager18@gmail.com>
2021-04-05 14:33:37 -0500

    Basic pasting functionality globally viewable.

--- html/js/js.js
+++ html/js/js.js
@@ -1,13 +1,63 @@
-window.addEventListener("paste", function(thePasteEvent){
-    // Use thePasteEvent object here ...
-    alert("You Pasted something? You called?");
-}, false);
+// Listen for paste events
+window.onload = function(){
+  document.getElementById("bodyContainer").onload = updatePage();
+  document.addEventListener('paste', (event) => {pasteContent()});
+}
+
+function updatePage(){
+  updateTimer = setInterval(function(){loadData()}, 1000);
+}
+
+function pasteContent(){
+  // navigator.permissions.query({name:'clipboard-read'}).then(function(result){
+  //   if(result.state=='granted'){console.log("Permission to paste allowed.")}
+  //   if(result.state=='prompt'){console.log("Permission to paste prompted?")}
+  //   if(result.state=='denied'){console.log("Permission to paste denied.")}
+  // });
+  navigator.clipboard.readText().then(text => addButton(text));
+}
 
 function copyText(context){
-    navigator.clipboard.writeText(this.innerHTML); 
+    //navigator.clipboard.writeText(this.innerHTML); 
     document.getElementById('tooltipCopied').style.transition = "0.25s";
     document.getElementById('tooltipCopied').style.display = 'flex';
-  }
-  function addButton(){
-    document.getElementById("bodyContainer").innerHTML+= "<button onhover='document.getElementById(\"btnDelete\").style.display=\"block\"; onclick=\"copyText(this)\">Blank Pasted Text</button>"
-  }
+}
+function addButton(pasteTxt){
+  //clipText = navigator.clipboard.readText()
+  console.log("Clipboard has: " + pasteTxt);
+  //htmlAdd = "<button id='pasteContent'>"+pasteTxt+"</button>"
+  //document.getElementById("bodyContainer").innerHTML+= htmlAdd;
+  postData(pasteTxt);
+  console.log("Button created from paste event");
+  //document.getElementById("bodyContainer").innerHTML+= "<button onhover='document.getElementById(\"btnDelete\").style.display=\"block\"; onclick=\"copyText(this)\">Blank Pasted Text</button>"
+}
+
+function postData(data){
+  // Send content to be upated to server.
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      //on responseText => add padding before being sent over & scan for <script> content
+      document.getElementById("updateContainer").innerHTML += this.responseText;
+    }
+    
+  };
+  xhttp.open("POST","/updateMainPage", true);
+  //xml can't set content length header
+  //xhttp.setRequestHeader("Content-Length", (new TextEncoder().encode(data)).length)
+  xhttp.send(data);
+}
+
+function loadData(){
+  // Load updated data from server.
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("updateContainer").innerHTML = this.responseText;
+    }
+  };
+  //Change this request to a post to keep everything temp instead of logging
+  // otherwise it will log that it's sending all the updated things to 
+  xhttp.open("GET","/updateMainPage", true);
+  xhttp.send();
+}
--- server.py
+++ server.py
@@ -1,18 +1,42 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from mimetypes import guess_type
-import os
+import os, sys
 # Handle connections for the Vaste server.
 # TODO
 # https://letsencrypt.org/docs/certificates-for-localhost/
 
+dynamicContent = [] # <-- Need to encrypt & decrypt later, but unencrypted for now
 
-def isCookieValid(self):
-    # is the users session ID in the valid & approved sessions
-    pass
+def fetchRecent(self):
+    # Fetch recent updates to the website from other users
+    self.send_header("Content-Length", sys.getsizeof(dynamicContent))
+    # add each item to it's own div if don't want to keep all html data in list
+    # ^^ might help prevent any js injection - that & casting the response
+    # to a string upon recieving it in the JS
+    response = ""
+    for f in dynamicContent:
+        response += (str(f)+"\n")
+    self.wfile.write(response.encode('utf-8'))
+
+def sendHeader(self, responseCode, contentLength):
+    if(type(responseCode)==int):
+        self.send_response(responseCode)
+        mimetype, _ = guess_type(self.path)
+        self.send_header("Content-type", mimetype)
+        self.send_header("X-Content-Type-Options", "nosniff")
+        self.send_header("Content-Length", contentLength)
+        self.end_headers()
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        isCookieValid(self)
+        # Update page contents, otherwise continue
+        if(self.path.endswith("/updateMainPage")):
+            # TODO:send headers here from sendHeader()
+            #sendHeader(self, 200, sys.getsizeof(dynamicContent))
+            self.send_response(200)
+            self.end_headers()
+            fetchRecent(self)
+            return
         self.protocol_version = "HTTP/1.1"
         file = ""
         if self.path.endswith("/"):
@@ -35,6 +59,27 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(file.read())
         file.close()
         return
+
+    def do_POST(self):
+        if(self.path.endswith("/updateMainPage")):
+            # Recieve the data sent
+            length = int(self.headers['Content-Length'])
+            print(f"reading length: {length}")
+            byteData = self.rfile.read(length)
+            data = byteData.decode("utf-8")
+            print(data)
+
+            # Response to the data send & update dynamicContent
+            # Add padding features for each button here ---
+            padding = "<div id='button'><h4>"
+            endPadding = "</h4></div>"
+            response = str(padding+data+endPadding)
+            dynamicContent.append(response)
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            self.wfile.write(response.encode('utf-8'))
+            return
 
 def run():
     HOST, PORT = "localhost", 8080
